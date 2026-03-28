deebaa0ae8fe50cbc2ad800290e822467a30b3f8
Karan pandey <100835737+karan-pandey15@users.noreply.github.com>
2026-03-29 00:50:56 +0530

    Add 'Deal of The day' option to product categories

--- src/app/pages/products/page.js
+++ src/app/pages/products/page.js
@@ -378,6 +378,8 @@ export default function CreateProduct() {
                     <option value="Snacks & drinks">Snacks & drinks </option>
                     <option value="Beauty & wellness">Beauty & wellness</option>
                     <option value="Ecommerce">Ecommerce</option>
+                    <option value="dealday">Deal of The day</option>
+
 
  
 
